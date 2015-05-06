4e5fdac5f28539784b6e04f3c0db7e34b8b6ec83
bgveenstra <b.g.veenstra@gmail.com>
2015-05-06 14:15:20 -0700

    added the required helper functions (skipped sort)

--- cash_register/my_utils.js
+++ cash_register/my_utils.js
@@ -8,18 +8,80 @@
 
     myUtils = {}
 
+
     myUtils.myEach = function myEach(arr, cb){
         var len = arr.length;
         for(var i=0; i<len; i++){
-            cb(arr[i], i, arr);
+            var cbresult = cb(arr[i], i, arr);
         }
     }
-    // myUtils.myMap
-    // myUtils.myReduce
-    
-    // myUtils.buildElement
-    
-    // myUtils.toDollarAmount;
-    // myUtils.toCurrencyString;
-    
+
+    myUtils.myMap = function myMap(arr, cb){
+      var len = arr.length;
+      var newArr = [];
+      for(var i=0; i<len; i++){
+         newArr[i] = cb(arr[i], i, arr);
+      }
+      return newArr;
+    }
+
+
+    myUtils.myReduce = function myReduce(arr, cb, val){
+      var len = arr.length;
+      val = val || 0;
+      for(var i=0; i<len; i++){
+         val = cb(val, arr[i], i, arr)
+      }
+      return val;
+    }
+
+   
+    myUtils.buildElement = function buildElement(tagType, innerStr, attrs){
+    //  takes in an HTML tag type and an inner HTML string 
+    //  update! and a JavaScript object containing any additional attributes
+    //  and outputs an HTML element as a string
+
+      // htmlString is actually an array we build up and then join
+      // start with the beginning of the opening tag
+      var htmlString = ["<", tagType];
+
+      // add htmlString fragments for each attribute in attrs
+      for (var key in attrs) {
+        if (attrs.hasOwnProperty(key)) {
+          htmlString.push(" ", key, "=", attrs[key]);
+        }
+      }
+
+      // add the end of the opening tag, the inner HTML string, 
+      // and the close tag
+      htmlString.push(">", innerStr, "</", tagType, ">");
+
+      // join everything up with empty separator and return
+      return htmlString.join("");
+    }
+
+    myUtils.toDollarAmount = function toDollarAmount(num){
+    // takes in a float and returns a currency amount
+    // (removes extra decimal places)
+        return (Math.round(num*100))/100;
+
+    }
+
+    myUtils.toCurrencyString = function toCurrencyString(num, symbol){
+    // takes in a float and a currency symbol 
+    // and returns a correctly formatted currency string
+        num = myUtils.toDollarAmount(num);
+        var currStr = num.toString();
+        var dotIndex = currStr.indexOf('.');
+        if (dotIndex === -1){
+            currStr = currStr + ".00";
+        } else if (dotIndex === currStr.length - 2){
+            currStr = currStr + "0";
+        } else {
+            currStr = currStr.substring(0, dotIndex+3); 
+        }
+        return symbol+currStr;
+    }
+
 }.call(this))
+
